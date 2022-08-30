7a51871c7f6ef2c2a67bd37d190ea9ce68921c78
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-08-30 11:29:18 -0300

    second commit

--- Django/navegador/views.py
+++ Django/navegador/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Olá mundo eu sou o navegador')
+
+
+def norte(request):
+    return render(request, 'navegador/norte.html')
